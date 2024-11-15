09e6688b5bd5dc28d1b1840635d0ba22d107d650
devSummon99 <michelperezsomoza@gmail.com>
2024-11-15 00:12:45 -0500

    server-ready

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("hello world");
+import express from "express";
+
+const app = express();
+
+app.get("/", (req,res) => {
+    res.send("<h1>Hello World</h1>");
+})
+
+app.listen(process.env.PORT || 3000 , () =>  {
+    console.log(`Server is listening on port ${3000}`);
+})
+
+export default app;
