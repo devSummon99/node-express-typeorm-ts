89141d29df0b21d27a1d1c4d8859f4e687406b1e
devSummon99 <michelperezsomoza@gmail.com>
2024-11-15 13:17:51 -0500

    install-pkgs

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import app from "./app";
 
-const app = express();
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cors());
 
 app.get("/", (req,res) => {
     res.send("<h1>Hello World</h1>");
 })
 
-app.listen(process.env.PORT || 3000 , () =>  {
-    console.log(`Server is listening on port ${3000}`);
+app.listen(process.env.PORT || 3001 , () =>  {
+    console.log(`Server on http://localhost:3001`);
 })
-
-export default app;
