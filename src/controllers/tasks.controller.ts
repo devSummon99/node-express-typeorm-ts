f950459b0b67b2d61d4a1c3c7028acedab071e88
devSummon99 <michelperezsomoza@gmail.com>
2024-11-19 17:05:14 -0500

    post-method-controller

--- src/controllers/tasks.controller.ts
+++ src/controllers/tasks.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express"
-import { taskRepository } from "../services/tasks.service";
+import { taskRepository, userRepository } from "../services";
+import { Task } from "../modules";
 
 export const getTasks = async (req: Request, res: Response) => {
     try {
@@ -17,10 +18,10 @@ export const getTaskByID = async (req: Request, res: Response) => {
     const { id } = req.params;
     try {
         const taskID = parseInt(id)
-       const task = await taskRepository.findOneBy({id: taskID})
-       if (task) 
-        res.status(200).json(task)
-       else { res.status(404).json({ msg: "Task no found" }); }
+        const task = await taskRepository.findOneBy({ id: taskID })
+        if (task)
+            res.status(200).json(task)
+        else { res.status(404).json({ msg: "Task no found" }); }
     } catch (err) {
         if (err instanceof Error)
             res.status(501).json(err.message)
@@ -28,7 +29,27 @@ export const getTaskByID = async (req: Request, res: Response) => {
 }
 
 export const createTask = async (req: Request, res: Response) => {
-    res.json("Creando tarea")
+    const { title, description, state, userID } = req.body;
+    try {
+        const newTask = new Task;
+        const taskFind = await taskRepository.findOne({ where: { title: title } })
+        if (taskFind) res.status(400).json({ msg: "Task has already been created" })
+        else {
+            const user = await userRepository.findOneBy({ id: userID });
+            if (!user) { res.status(404).json({ msg: "User no found" }) }
+            else {
+                newTask.title = title;
+                newTask.description = description;
+                newTask.state = state;
+                newTask.user = user;
+                taskRepository.save(newTask);
+                res.status(201).json("Task created")
+            }
+        }
+    } catch (err) {
+        if (err instanceof Error)
+            res.status(501).json(err.message)
+    }
 }
 
 export const updateTask = async (req: Request, res: Response) => {
