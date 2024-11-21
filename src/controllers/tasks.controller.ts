6d31126f20be1b6c9b4dc6c522c5da2eee71be0b
devSummon99 <michelperezsomoza@gmail.com>
2024-11-21 00:22:23 -0500

    task-controller-remove

--- src/controllers/tasks.controller.ts
+++ src/controllers/tasks.controller.ts
@@ -7,71 +7,83 @@ export const getTasks = async (req: Request, res: Response) => {
         const tasks = await taskRepository.find();
         if (tasks.length > 0)
             res.status(200).json(tasks);
-        else { res.status(404).json({ msg: "Tasks no found" }); }
+        else { res.status(404).json("Tasks no found"); }
     } catch (err) {
         if (err instanceof Error)
-            res.status(501).json(err.message)
+            res.status(501).json(err.message);
     }
 }
 
 export const getTaskByID = async (req: Request, res: Response) => {
     const { id } = req.params;
     try {
-        const taskID = parseInt(id)
-        const task = await taskRepository.findOneBy({ id: taskID })
+        const taskID = parseInt(id);
+        const task = await taskRepository.findOneBy({ id: taskID });
         if (task)
-            res.status(200).json(task)
-        else { res.status(404).json({ msg: "Task no found" }); }
+            res.status(200).json(task);
+        else { res.status(404).json("Task no found"); }
     } catch (err) {
         if (err instanceof Error)
-            res.status(501).json(err.message)
+            res.status(501).json(err.message);
     }
 }
 
 export const createTask = async (req: Request, res: Response) => {
     const { title, description, state, userID } = req.body;
     try {
-        const taskFind = await taskRepository.findOne({ where: { title: title } })
-        if (taskFind) res.status(400).json({ msg: "Task has already been created" })
+        const taskFind = await taskRepository.findOne({ where: { title: title } });
+        if (taskFind) res.status(400).json("Task has already been created");
         else {
             const newTask = new Task;
             const user = await userRepository.findOneBy({ id: userID });
             newTask.title = title;
             newTask.description = description;
             newTask.state = state;
-            if (!user) { res.status(404).json({ msg: "User no found" }) }
+            if (!user) { res.status(404).json("User no found"); }
             else { newTask.user = user; }
             taskRepository.save(newTask);
-            res.status(201).json("Task created")
+            res.status(201).json("Task created");
         }
     } catch (err) {
         if (err instanceof Error)
-            res.status(501).json(err.message)
+            res.status(501).json(err.message);
     }
 }
 
 export const updateTask = async (req: Request, res: Response) => {
     const { id } = req.params;
     try {
-        const taskID = parseInt(id)
-        const task = await taskRepository.findOneBy({ id: taskID })
+        const taskID = parseInt(id);
+        const task = await taskRepository.findOneBy({ id: taskID });
         if (task) {
             const { title, description, state } = req.body;
-            await taskRepository.update({id: taskID},{ title, description, state })
-            res.status(203).json("Task has been modified successfully ")
+            await taskRepository.update({ id: taskID }, { title, description, state });
+            res.status(203).json("Task has been modified successfully ");
         }
         else {
-            res.status(404).json({ msg: "Task no found" });
+            res.status(404).json("Task no found");
         }
     } catch (err) {
         if (err instanceof Error)
-            res.status(501).json(err.message)
+            res.status(501).json(err.message);
     }
 }
 
 
 export const deleteTask = async (req: Request, res: Response) => {
     const { id } = req.params;
-    res.json(`Eliminando tarea numero ${id}`)
+    try {
+        const taskID = parseInt(id);
+        const task = await taskRepository.findOneBy({ id: taskID });
+        if (task) {
+            await taskRepository.delete({ id: taskID });
+            res.status(203).json("Task has been deleted");
+        } else {
+            res.status(404).json("Task no found");
+        }
+    } catch (err) {
+        if (err instanceof Error)
+            res.status(501).json(err.message);
+    }
 }
 
