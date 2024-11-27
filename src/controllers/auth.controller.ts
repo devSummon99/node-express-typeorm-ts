c928a35578cee21c17847bf5f7426c0ff25fb695
devSummon99 <michelperezsomoza@gmail.com>
2024-11-26 23:50:43 -0500

    auth-req-res

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -2,10 +2,10 @@ import { Request, Response } from "express"
 import { userRepository } from "../services";
 import { User } from "../modules";
 
-export const login = () => {
+export const login = async (req: Request, res: Response) => {
 
 }
 
-export const register = () => {
+export const register = async (req: Request, res: Response) => {
     
 }
