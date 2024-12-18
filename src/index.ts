d6c0c6da220e67ccefd098172303e7c868d2a96b
devSummon99 <michelperezsomoza@gmail.com>
2024-12-18 11:35:23 -0500

    errorMiddleware

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
-
 import { userRoutes, taskRoutes, authRoutes } from "./routes/";
 import app from "./app";
 import { db } from "./db";
+import { errorMiddleware } from "./middleware";
 
 
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cors());
+app.use(errorMiddleware)
 
 app.get("/", (req, res) => {
     res.send("<h1>Hello World</h1>");
